2345d944fb283aee1d8e1a115c38e23e28a0db52
YuvCarl <carldrake969@gmail.com>
2022-09-02 11:08:58 +0100

    updated the file

---
+++ app.js
@@ -0,0 +1,11 @@
+var rate = document.querySelectorAll('span');
+
+
+ChangeColor =() =>{
+    let rate = document.querySelectorAll('rate');
+
+    for(var i = 0; i<= 5; i++){
+        document.querySelectorAll('rate:active').style.backgroundColor = 'hsl(25, 97%, 53%)';
+    }
+    
+} 
