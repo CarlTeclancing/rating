04191c3900ce01b5c0309fdb44e0343bc11d093e
YuvCarl <carldrake969@gmail.com>
2022-09-02 16:35:44 +0100

    added updated app.js and style.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let rate4 = document.getElementById('rate4');
 let rate5 = document.getElementById('rate5');
 let btn = document.getElementById('btn-submit');
 
-let x;
+let x = 0;
 
 rate1.addEventListener('click', function(){
     x = 1;
@@ -28,40 +28,46 @@ rate2.addEventListener('click', function(){
 
 rate3.addEventListener('click', function(){
     x = 3;
-    rate1.style.background = 'gray'
-    rate2.style.background = 'gray'
-    rate3.style.background = 'hsl(25, 97%, 53%)'
-    rate4.style.background = 'gray'
-    rate5.style.background = 'gray'
+    rate1.style.background = 'gray';
+    rate2.style.background = 'gray';
+    rate3.style.background = 'hsl(25, 97%, 53%)';
+    rate4.style.background = 'gray';
+    rate5.style.background = 'gray';
 })
 
 rate4.addEventListener('click', function(){
     x = 4;
-    rate1.style.background = 'gray'
-    rate2.style.background = 'gray'
-    rate3.style.background = 'gray'
-    rate4.style.background = 'hsl(25, 97%, 53%)'
-    rate5.style.background = 'gray'
+    rate1.style.background = 'gray';
+    rate2.style.background = 'gray';
+    rate3.style.background = 'gray';
+    rate4.style.background = 'hsl(25, 97%, 53%)';
+    rate5.style.background = 'gray';
 })
 
 rate5.addEventListener('click', function(){
     x = 5;
-    rate1.style.background = 'gray'
-    rate2.style.background = 'gray'
-    rate3.style.background = 'gray'
-    rate4.style.background = 'gray'
-    rate5.style.background = 'hsl(25, 97%, 53%)'
+    rate1.style.background = 'gray';
+    rate2.style.background = 'gray';
+    rate3.style.background = 'gray';
+    rate4.style.background = 'gray';
+    rate5.style.background = 'hsl(25, 97%, 53%)';
 })
 
 
 
 btn.addEventListener('click', function(){
-    
+    document.getElementById('txt').innerHTML = ('You selected ' + x + ' ouT of 5')
     document.getElementById('card').style.display = 'none';
     document.getElementById('card1').style.display = 'flex';
-    rate1.style.background = 'gray'
-    rate2.style.background = 'gray'
-    rate3.style.background = 'gray'
-    rate4.style.background = 'gray'
-    rate5.style.background = 'gray'
+    rate1.style.background = 'gray';
+    rate2.style.background = 'gray';
+    rate3.style.background = 'gray';
+    rate4.style.background = 'gray';
+    rate5.style.background = 'gray';
+
+    setInterval(() => {
+        alert('Check us on YT CODEwithCREST')
+    }, 30000);
+
+    
 })
