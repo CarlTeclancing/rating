60a02b3a66372d1f718a93d34ef4867bd2cdf8c3
YuvCarl <carldrake969@gmail.com>
2022-09-02 16:04:31 +0100

    updates on the cart

--- app.js
+++ app.js
@@ -1,11 +1,65 @@
 var rate = document.querySelectorAll('span');
+let rate1 = document.getElementById('rate1');
+let rate2 = document.getElementById('rate2');
+let rate3 = document.getElementById('rate3');
+let rate4 = document.getElementById('rate4');
+let rate5 = document.getElementById('rate5');
+let btn = document.getElementById('btn-submit');
+
+let x;
+
+rate1.addEventListener('click', function(){
+    x = 1;
+    rate1.style.background = 'hsl(25, 97%, 53%)'
+    rate2.style.background = 'gray'
+    rate3.style.background = 'gray'
+    rate4.style.background = 'gray'
+    rate5.style.background = 'gray'
+})
+
+rate2.addEventListener('click', function(){
+    x = 2;
+    rate1.style.background = 'gray'
+    rate2.style.background = 'hsl(25, 97%, 53%)'
+    rate3.style.background = 'gray'
+    rate4.style.background = 'gray'
+    rate5.style.background = 'gray'
+})
+
+rate3.addEventListener('click', function(){
+    x = 3;
+    rate1.style.background = 'gray'
+    rate2.style.background = 'gray'
+    rate3.style.background = 'hsl(25, 97%, 53%)'
+    rate4.style.background = 'gray'
+    rate5.style.background = 'gray'
+})
+
+rate4.addEventListener('click', function(){
+    x = 4;
+    rate1.style.background = 'gray'
+    rate2.style.background = 'gray'
+    rate3.style.background = 'gray'
+    rate4.style.background = 'hsl(25, 97%, 53%)'
+    rate5.style.background = 'gray'
+})
+
+rate5.addEventListener('click', function(){
+    x = 5;
+    rate1.style.background = 'gray'
+    rate2.style.background = 'gray'
+    rate3.style.background = 'gray'
+    rate4.style.background = 'gray'
+    rate5.style.background = 'hsl(25, 97%, 53%)'
+})
 
 
-ChangeColor =() =>{
-    let rate = document.querySelectorAll('rate');
 
-    for(var i = 0; i<= 5; i++){
-        document.querySelectorAll('rate:active').style.backgroundColor = 'hsl(25, 97%, 53%)';
-    }
-    
-} 
+btn.addEventListener('click', function(){
+    alert(x)
+    rate1.style.background = 'gray'
+    rate2.style.background = 'gray'
+    rate3.style.background = 'gray'
+    rate4.style.background = 'gray'
+    rate5.style.background = 'gray'
+})
